21e65d1db76abb4a9bd1884817055883c9054da3
Julius0505 <se7en.mini.123@protonmail.com>
2021-12-17 05:25:31 +0300

    add drawer, player controller working

--- app/src/components/tagging/index.js
+++ app/src/components/tagging/index.js
@@ -1,38 +1,96 @@
 import * as React from 'react';
-import { useParams } from "react-router-dom";
+import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
+import Drawer from '@mui/material/Drawer';
+import CssBaseline from '@mui/material/CssBaseline';
+import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import IconButton from '@mui/material/IconButton';
+
+import { useParams } from "react-router-dom";
 import VideoPlayer from './videoplayer';
-import Playground from './playground';
 import CryptoJS from 'crypto-js'
 import { SECRET } from "../../config/settings"
 import GameService from '../../services/game.service';
+const drawerWidth = "30%";
+
+const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
+  ({ theme, open }) => ({
+    flexGrow: 1,
+    display: 'flex',
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: `-${drawerWidth}`,
+    ...(open && {
+      transition: theme.transitions.create('margin', {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      marginLeft: 0,
+    }),
+  }),
+);
 
 export default function Tagging() {
-	const { id } = useParams();
+  const { id } = useParams();
 
-	const [url, setUrl] = React.useState("");
+  const [url, setUrl] = React.useState("");
 
-	React.useEffect(() => {
-		const game_id = CryptoJS.AES.decrypt(id, SECRET).toString(CryptoJS.enc.Utf8)
-		console.log("Game id", game_id)
-		GameService.getGame(game_id).then((res) => {
-			console.log("game Data", res);
-			setUrl(res.video_url);
-		})
+  React.useEffect(() => {
+    const game_id = CryptoJS.AES.decrypt(id, SECRET).toString(CryptoJS.enc.Utf8)
+    console.log("Game id", game_id)
+    GameService.getGame(game_id).then((res) => {
+      console.log("game Data", res);
+      setUrl(res.video_url);
+    })
 
-	}, [id])
+  }, [id])
 
-	return (
-		<Box sx={{ flexGrow: 1 }}>
-			<Grid container spacing={2}>
-				<Grid item xs={12} sm={4}>
-					<Playground />
-				</Grid>
-				<Grid item xs={12} sm={8}>
-					<VideoPlayer url={url}/>
-				</Grid>
-			</Grid>
-		</Box>
-	);
+  const [open, setOpen] = React.useState(true);
+
+  const handleDrawerOpen = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <Drawer
+        sx={{
+          width: "30%",
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: "30%",
+            boxSizing: 'border-box',
+          },
+        }}
+        variant="persistent"
+        anchor="left"
+        open={open}
+      >
+        Hello there!
+        here is drawer
+      </Drawer>
+      <Main open={open}>
+        <div style={{width:50}}>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            sx={{height: 50, width: 50, position: 'fixed', zIndex: 999999, top: '45%', border: 2, }}
+          >
+            {open ? <KeyboardArrowLeftIcon/> : <KeyboardArrowRightIcon/>}
+          </IconButton>
+        </div>
+        <Box sx={{ flexGrow: 1 }}>
+          <VideoPlayer url={url} />
+          <div>
+          </div>
+        </Box>
+      </Main>
+    </Box>
+  );
 }
--- app/src/components/tagging/videoplayer.js
+++ app/src/components/tagging/videoplayer.js
@@ -2,48 +2,59 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import PlayCircleOutlineIcon from '@mui/icons-material/PlayCircleOutline';
-// import PauseCircleOutlineIcon from '@mui/icons-material/PauseCircleOutline';
+import PauseCircleOutlineIcon from '@mui/icons-material/PauseCircleOutline';
 import ReactPlayer from 'react-player';
 
-export default function VideoPlayer({url}) {
+export default function VideoPlayer({ url }) {
+  const [play, setPlay] = React.useState(false)
+  const player = React.useRef(null);
+
+  const seekTo = (sec) => {
+    player.current.seekTo(player.current.getCurrentTime() + sec)
+  } 
+
   return (
-    <div>
+    <div width='100%' height='100%'>
       <ReactPlayer
         url={url}
-        playing={true}
+        ref={player}
+        playing={play}
         // controls={true}
         width='100%'
         height='100%'
       />
-      <Box sx={{ flexGrow: 1 }}>
-        <Button variant="outlined" startIcon={<PlayCircleOutlineIcon />}>
-          Play
-        </Button>
-        {/* <Button variant="outlined" startIcon={<PauseCircleOutlineIcon />}>
-							Pause
-						</Button> */}
-        <Button variant="outlined" >
+      <Box sx={{ flexGrow: 1, textAlign: 'center' }}>
+        {play ?
+          <Button variant="outlined" sx={{width:100}} startIcon={<PauseCircleOutlineIcon />} onClick={() => setPlay(false)}>
+            Pause
+          </Button>
+          :
+          <Button variant="outlined" sx={{width:100}} startIcon={<PlayCircleOutlineIcon />} onClick={() => setPlay(true)}>
+            Play
+          </Button>
+        }
+        <Button variant="outlined" onClick={() => seekTo(-10)}>
           -10s
         </Button>
-        <Button variant="outlined" >
+        <Button variant="outlined" onClick={() => seekTo(-5)}>
           -5s
         </Button>
-        <Button variant="outlined" >
+        <Button variant="outlined" onClick={() => seekTo(-3)}>
           -3s
         </Button>
-        <Button variant="outlined" >
+        <Button variant="outlined" onClick={() => seekTo(-1)}>
           -1s
         </Button>
-        <Button variant="outlined" >
+        <Button variant="outlined" onClick={() => seekTo(1)}>
           +1s
         </Button>
-        <Button variant="outlined" >
+        <Button variant="outlined" onClick={() => seekTo(3)}>
           +3s
         </Button>
-        <Button variant="outlined" >
+        <Button variant="outlined" onClick={() => seekTo(5)}>
           +5s
         </Button>
-        <Button variant="outlined" >
+        <Button variant="outlined" onClick={() => seekTo(10)}>
           +10s
         </Button>
       </Box>
