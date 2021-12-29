89dc0152f92bb83e549607a44253687db7669605
Julius0505 <se7en.mini.123@protonmail.com>
2021-12-29 13:54:53 +0300

    remove defensive players and result list in SHORT PASS

--- app/src/components/tagging/contents/ShortPass.js
+++ app/src/components/tagging/contents/ShortPass.js
@@ -27,34 +27,6 @@ export default function ShortPass({
   offenseTeamId, 
 }) {
 
-  const [result, setResult] = React.useState({ id: 4, name: SUCCESSFUL })
-  const [offensivePlayer, setOffensivePlayer] = React.useState(offenseTeam[0]);
-  const [defensivePlayer, setDefensivePlayer] = React.useState({});
-
-  const tagData = {
-    start_time: startTime,
-    end_time: endTime
-  }
-
-  const defensivePlayerClicked = (player) => {
-    setDefensivePlayer(player)
-    taggingState([{
-      ...tagData,
-      team_id: defenseTeamId,
-      player_id: player.id,
-      action_id: 14,
-      action_type_id: 4, 
-      action_result_id: 11
-    },{
-      ...tagData,
-      player_id: offensivePlayer.id,
-      team_id: offenseTeamId,
-      action_id: 10,
-      action_type_id: 4, 
-      action_result_id: 11
-    }])
-  }
-
   return (
     <>
       <SubBox>
@@ -62,8 +34,17 @@ export default function ShortPass({
           {
             offenseTeam.map((player, i) => (
               <ListItemButton key={i}
-                selected={offensivePlayer === player}
-                onClick={() => setOffensivePlayer( player )}
+                onClick={() => 
+                  taggingState([{
+                    start_time: startTime,
+                    end_time: endTime,
+                    team_id: offenseTeamId,
+                    player_id: player.id,
+                    action_type_id: 4, 
+                    action_result_id: 4, 
+                    action_id: 2
+                  }])
+                }
               >
                 <ListItemText primary={`${player.f_name} ${player.l_name}  #${player.jersey_number}  (${player.date_of_birth && player.date_of_birth.slice(0, 10)})`} />
               </ListItemButton>
@@ -71,48 +52,6 @@ export default function ShortPass({
           }
         </List>
       </SubBox>
-
-      <SubBox>
-        <List header="Result">
-          {[
-            { id: 4, name: SUCCESSFUL },
-            { id: 5, name: STOLE_BY },
-            { id: 11, name: BAD_PASS },
-          ].map((r, i) => (
-            <ListItemButton key={r.id}
-              selected={result?.id === r.id}
-              onClick={() => {
-                setResult(r) 
-                const d = {
-                  ...tagData,
-                  team_id: offenseTeamId,
-                  player_id: offensivePlayer.id,
-                  action_type_id: 4, 
-                  action_result_id: r.id
-                };
-                if(r.name === SUCCESSFUL) taggingState([{...d, action_id: 2,}])
-                if(r.name === BAD_PASS) taggingState([{...d, action_id: 10,}])
-              }}
-            >
-              <ListItemText primary={r.name} />
-            </ListItemButton>
-          ))}
-        </List>
-      </SubBox>
-      {result.name === STOLE_BY && <SubBox>
-        <List header="Defensive Player List">
-          {
-            defenseTeam.map((player, i) => (
-              <ListItemButton key={i}
-                selected={defensivePlayer === player}
-                onClick={() => defensivePlayerClicked(player)}
-              >
-                <ListItemText primary={`${player.f_name} ${player.l_name}  #${player.jersey_number}  (${player.date_of_birth && player.date_of_birth.slice(0, 10)})`} />
-              </ListItemButton>
-            ))
-          }
-        </List>
-      </SubBox>}
     </>
   );
 } 
