9e544a3b9452692377f88387d10eba6a64ba9494
Julius0505 <se7en.mini.123@protonmail.com>
2022-04-06 14:27:46 +0300

    fix font style

--- app/src/components/coach/Tabs/PlayerStatsTab/index.js
+++ app/src/components/coach/Tabs/PlayerStatsTab/index.js
@@ -151,7 +151,7 @@ const PlayerStatsTab = ({ player }) => {
                     {RULE.filter(f => !f.opponent).map((rule, idx) => {
                         let sum_success = 0, sum_unsuccess = 0
                         return <Card sx={{ fontSize: "0.8rem", position: "relative", margin: "0.5rem", maxWidth: 500, minWidth: 320, display: "inline-block", verticalAlign: "top" }}>
-                            <Typography sx={{ textAlign: 'center', backgroundColor: 'lightgray' }}>{rule.title}</Typography>
+                            <Typography sx={{ textAlign: 'center', backgroundColor: 'lightgray', fontWeight: "bold", textTransform: "uppercase" }}>{rule.title}</Typography>
                             <Table responsive="sm" striped borderless hover size="sm" className='text-uppercase coach-actionlist-table'>
                                 <tbody className='text-center' style={{ m: 0 }}>
                                     {!!rule?.successful && <tr>
