2a3cf8575b1d41adc8a3d51e4995847eba913cbd
Julius0505 <se7en.mini.123@protonmail.com>
2022-04-08 15:59:15 +0300

    fix

--- app/src/common/utilities.js
+++ app/src/common/utilities.js
@@ -8,7 +8,7 @@ export const createCommand = (tagList, url, name) => {
   const command = `ffmpeg -y -i "${url}" -f lavfi -i color=color=#808080@0.7:size=240x60,format=rgba -f lavfi -i color=color=#FFA500@0.7:size=240x60,format=rgba -i "https://s3.eu-west-1.amazonaws.com/scouting4u.com/IMG/JustSmallLogo.png" -filter_complex "${tagList.map((tag, i) =>
     `[0:v]trim=${toSecond(tag.start_time)}:${toSecond(tag.end_time)},setpts=PTS-STARTPTS[v${i}];[1:v]drawtext=text='${tag.action_name ?? "TEAM_ACTION"}':fontfile=ArialBold.ttf:x=(w-text_w)/2:y=(h-text_h)/2:fontsize=40:fontcolor=white[trans_gray_tx${i ? i : ''}];[v${i}][trans_gray_tx${i ? i : ''}]overlay=shortest=1:x=20:y=H-h-20[v_${i}];[0:a]atrim=${toSecond(tag.start_time)}:${toSecond(tag.end_time)},asetpts=PTS-STARTPTS[a${i}];`).join("")
     }${tagList.map((tag, i) => `[v_${i}][a${i}]`).join("")
-    }concat=n=${tagList.length}:v=1:a=1[outv][outa];[2:v]drawtext=text='# ${tagList[0]?.player_fname ?? name}':fontfile=ArialBold.ttf:x=(w-text_w)/2:y=(h-text_h)/2:fontsize=40:fontcolor=white[trans_org_tx];[outv][trans_org_tx]overlay=shortest=1:x=262:y=H-h-20[v_org];[v_org][3:v]overlay=W-w-20:H-h-20[v_t_logo]" -map "[v_t_logo]" -map "[outa]" -preset fast "${name}.mp4"`
+    }concat=n=${tagList.length}:v=1:a=1[outv][outa];[2:v]drawtext=text='${!tagList[0]?.player_fname ? name : `#${agList[0]?.jersey_number} ${agList[0]?.player_fname} ${agList[0]?.player_lname}`}':fontfile=ArialBold.ttf:x=(w-text_w)/2:y=(h-text_h)/2:fontsize=40:fontcolor=white[trans_org_tx];[outv][trans_org_tx]overlay=shortest=1:x=262:y=H-h-20[v_org];[v_org][3:v]overlay=W-w-20:H-h-20[v_t_logo]" -map "[v_t_logo]" -map "[outa]" -preset fast "${name}.mp4"`
 
   fileDownload(command, `${name}.bat`);
 }
