90714badf37fe3c0d92dea946b5a3ca5ba5e63b1
Scouting4U2010 <Scouting4U2010@gmail.com>
2022-08-16 19:30:41 +0300

    settins page corrections

--- app/src/components/newcoach/settings/components.js
+++ app/src/components/newcoach/settings/components.js
@@ -1,26 +1,47 @@
-import { Button, InputBase } from '@mui/material';
-import { styled, alpha } from '@mui/material/styles';
+import { Button, InputBase, TextField } from '@mui/material';
+import { styled } from '@mui/material/styles';
+
+const StyleTextField = styled(TextField)(({ theme }) => ({
+    '& label.Mui-focused': {
+        color: '#1A1B1D'
+    },
+    '& .MuiInput-underline:after': {
+        borderBottomColor: '#1A1B1D'
+    },
+    '& .MuiOutlinedInput-root': {
+        '& fieldset': {
+            borderColor: '#1A1B1D'
+        },
+        '&:hover fieldset': {
+            borderColor: '#1A1B1D'
+        },
+        '&.Mui-focused fieldset': {
+            borderColor: '#1A1B1D'
+        }
+    },
+    '& .MuiInputBase-input': {
+        backgroundColor: 'white'
+    }
+}));
 
 const BootstrapInput = styled(InputBase)(({ theme }) => ({
     'label + &': {
         marginTop: theme.spacing(3)
     },
     '& .MuiInputBase-input': {
-        borderRadius: '8px',
-        position: 'relative',
-        backgroundColor: theme.palette.mode === 'light' ? 'white' : '#2b2b2b',
-        border: '1px solid #ced4da',
-        fontSize: '16px',
         width: '300px',
         height: '48px',
-        padding: '0 12px',
-        transition: theme.transitions.create(['border-color', 'background-color', 'box-shadow']),
-        fontFamily: ['-apple-system', 'BlinkMacSystemFont', '"Segoe UI"', 'Roboto', '"Helvetica Neue"', 'Arial', 'sans-serif', '"Apple Color Emoji"', '"Segoe UI Emoji"', '"Segoe UI Symbol"'].join(
-            ','
-        ),
+        borderRadius: '10px',
+        position: 'relative',
+        backgroundColor: theme.palette.mode === 'light' ? 'white' : '#2b2b2b',
+        border: '2px solid #e8e8e8',
+        fontSize: '14px',
+        lineHeight: '20px',
+        padding: '0 16px',
+        transition: 'border-color 0.15s ease-in-out 0s',
+        fontFamily: ['DM Sans', 'sans-serif'].join(','),
         '&:focus': {
-            boxShadow: `${alpha(theme.palette.primary.main, 0.25)} 0 0 0 0.2rem`,
-            borderColor: theme.palette.primary.dark
+            borderColor: '#1A1B1D'
         }
     }
 }));
@@ -29,12 +50,13 @@ const SaveButton = styled(Button)(({ theme }) => ({
     borderRadius: '10px',
     height: '48px',
     textTransform: 'capitalize',
-    fontFamily: 'sans-serif',
+    fontFamily: "'DM Sans', sans-serif",
     fontWeight: 600,
+    fontSize: '14px',
     backgroundColor: '#fe5e00',
     color: 'white',
     '&:disabled': { backgroundColor: '#e8e8e8' },
     '&:hover': { backgroundColor: 'orange' }
 }));
 
-export { BootstrapInput, SaveButton };
+export { StyleTextField, BootstrapInput, SaveButton };
