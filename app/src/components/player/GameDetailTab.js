298e15fa2a5592355888ae11ea774f5b48c7f01a
smart9205 <oranvitaliano@gmail.com>
2022-10-19 13:56:31 +0300

    changed translation

--- app/src/components/player/GameDetailTab.js
+++ app/src/components/player/GameDetailTab.js
@@ -41,7 +41,7 @@ export default function GameDetailTab({ playTags, gameList, t }) {
                     <Tabs value={value} onChange={handleChange} aria-label="basic tabs" centered>
                         <Tab label={t('Games')} {...a11yProps(0)} />
                         <Tab label={t('Skills')} {...a11yProps(1)} />
-                        <Tab label={t('Statistics')} {...a11yProps(2)} />
+                        <Tab label={t('Season Statistics')} {...a11yProps(2)} />
                         {showHighlight && <Tab label={t('Highlights')} {...a11yProps(3)} />}
                     </Tabs>
                 </Box>
