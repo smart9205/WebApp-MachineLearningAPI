c1688087c9ab4cb92b6351e1eb81808de1951078
smart9205 <oranvitaliano@gmail.com>
2022-11-10 21:45:46 +0200

    Tagging tool correction

--- app/src/components/tagging/contents/GK.js
+++ app/src/components/tagging/contents/GK.js
@@ -29,17 +29,22 @@ export default function GK({ defenseTeam, offenseTeam, taggingState, offenseTeam
         <>
             <AreaCourtMenu areaCourtId={areaCourtId} setAreaCourtId={setAreaCourtId} inTheBox={inTheBox} setInTheBox={setInTheBox} />
 
-            <PlayerSelector title="Goalkeepers" playerList={defenseTeamGoalKeeper} editable={false} selected={defensivePlayer} onSelect={(player) => setDefensivePlayer(player)} />
-
+            <PlayerSelector title="Defensive Goalkeepers" playerList={defenseTeamGoalKeeper} editable={false} selected={defensivePlayer} onSelect={(player) => setDefensivePlayer(player)} />
             <SubBox>
                 <List header="Type">
                     {[
                         { id: 18, name: 'Air Challenge' },
                         { id: 19, name: 'Ground Challenge' },
-                        { id: 20, name: 'One vs One' },
-                    ].map((type, i) => (
-                        <ListItemButton key={i} selected={actionTypeId === type.id} onClick={() => setActionTypeId(type.id)}>
-                            <ListItemText primary={type.name} />
+                        { id: 20, name: 'One vs One' }
+                    ].map((r, i) => (
+                        <ListItemButton
+                            key={r.id}
+                            selected={actionTypeId === r.id}
+                            onClick={() => {
+                                setActionTypeId(r.id);
+                            }}
+                        >
+                            <ListItemText primary={r.name} />
                         </ListItemButton>
                     ))}
                 </List>
@@ -53,93 +58,19 @@ export default function GK({ defenseTeam, offenseTeam, taggingState, offenseTeam
                     ].map((r, i) => (
                         <ListItemButton
                             key={r.id}
-                            selected={result === r}
+                            selected={result?.id === r.id}
                             onClick={() => {
                                 setResult(r);
-                                if(actionTypeId === 18){
-                                    if (r.name === 'Successful') {
-                                        taggingState([
-                                            {
-                                                action_type_id: actionTypeId,
-                                                team_id: defenseTeamId,
-                                                player_id: defensivePlayer.id,
-                                                action_id: actionTypeId,
-                                                action_result_id: r.id,
-                                                court_area_id: areaCourtId,
-                                                inside_the_paint: inTheBox
-                                            }
-                                        ]);
-                                    }
-                                    if (r.name === 'Unsuccessful') {
-                                        taggingState([
-                                            {
-                                                action_type_id: actionTypeId,
-                                                team_id: defenseTeamId,
-                                                player_id: defensivePlayer.id,
-                                                action_id: actionTypeId,
-                                                action_result_id: r.id,
-                                                court_area_id: areaCourtId,
-                                                inside_the_paint: inTheBox
-                                            }
-                                        ]);
-                                    }
-                                }
-                                if(actionTypeId === 19){
-                                    if (r.name === 'Successful') {
-                                        taggingState([
-                                            {
-                                                action_type_id: actionTypeId,
-                                                team_id: defenseTeamId,
-                                                player_id: defensivePlayer.id,
-                                                action_id: actionTypeId,
-                                                action_result_id: r.id,
-                                                court_area_id: areaCourtId,
-                                                inside_the_paint: inTheBox
-                                            }
-                                        ]);
-                                    }
-                                    if (r.name === 'Unsuccessful') {
-                                        taggingState([
-                                            {
-                                                action_type_id: actionTypeId,
-                                                team_id: defenseTeamId,
-                                                player_id: defensivePlayer.id,
-                                                action_id: actionTypeId,
-                                                action_result_id: r.id,
-                                                court_area_id: areaCourtId,
-                                                inside_the_paint: inTheBox
-                                            }
-                                        ]);
-                                    }
-                                }
-                                if(actionTypeId === 20){
-                                    if (r.name === 'Successful') {
-                                        taggingState([
-                                            {
-                                                action_type_id: actionTypeId,
-                                                team_id: defenseTeamId,
-                                                player_id: defensivePlayer.id,
-                                                action_id: actionTypeId,
-                                                action_result_id: r.id,
-                                                court_area_id: areaCourtId,
-                                                inside_the_paint: inTheBox
-                                            }
-                                        ]);
-                                    }
-                                    if (r.name === 'Unsuccessful') {
-                                        taggingState([
-                                            {
-                                                action_type_id: actionTypeId,
-                                                team_id: defenseTeamId,
-                                                player_id: defensivePlayer.id,
-                                                action_id: actionTypeId,
-                                                action_result_id: r.id,
-                                                court_area_id: areaCourtId,
-                                                inside_the_paint: inTheBox
-                                            }
-                                        ]);
-                                    }
-                                }
+                                const d = {
+                                    action_type_id: 8,
+                                    team_id: defenseTeamId,
+                                    player_id: defensivePlayer.id,
+                                    action_result_id: r.id,
+                                    court_area_id: areaCourtId,
+                                    inside_the_paint: inTheBox
+                                };
+                                if (r.name === 'Successful') taggingState([{ ...d, action_id: actionTypeId }]);
+                                if (r.name === 'Unsuccessful') taggingState([{ ...d, action_id: actionTypeId }]);
                             }}
                         >
                             <ListItemText primary={r.name} />
@@ -147,7 +78,6 @@ export default function GK({ defenseTeam, offenseTeam, taggingState, offenseTeam
                     ))}
                 </List>
             </SubBox>
-
         </>
     );
 }
