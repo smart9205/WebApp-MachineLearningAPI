9501a8df86fe31cc6ca36ceae285a66b87af2e82
smart9205 <oranvitaliano@gmail.com>
2022-11-13 05:55:01 +0200

    team page design

--- app/src/components/newcoach/teams/tabs/stats/videoDialog.js
+++ app/src/components/newcoach/teams/tabs/stats/videoDialog.js
@@ -101,11 +101,11 @@ const TeamStatsVideoPlayer = ({ open, onClose, video_url, tagList }) => {
     console.log('Daniel====', currentIndex);
 
     return (
-        <Dialog style={{ minWidth: '100%', backgroundColor: 'transparent' }} className="profileSection_tagvideo" open={open} onClose={() => onClose(updated > 0)}>
+        <Dialog style={{ minWidth: '98%', backgroundColor: 'transparent' }} className="profileSection_tagvideo" open={open} onClose={() => onClose(updated > 0)}>
             <DialogContent style={{ p: 0 }}>
-                <div style={{ width: '95%', margin: 'auto', position: 'relative' }}>
+                <div style={{ width: '100%', margin: 'auto', position: 'relative' }}>
                     <FullScreen handle={handle}>
-                        <div style={{ width: '100%', margin: 'auto' }}>
+                        <div style={{ width: '80%', margin: 'auto' }}>
                             <div className="player-wrapper">
                                 <ReactPlayer 
                                     className="react-player"
