c9c273ea3263ac3fb90da65ae59acad1a290bd70
smart9205 <oranvitaliano@gmail.com>
2022-12-08 22:52:20 +0200

    New roles and menus

--- app/src/components/newcoach/settings/representative/academyTeamGame.js
+++ app/src/components/newcoach/settings/representative/academyTeamGame.js
@@ -1,5 +1,5 @@
 import { CircularProgress, IconButton } from '@mui/material';
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import CloseIcon from '@mui/icons-material/DisabledByDefault';
 import CheckBoxIcon from '@mui/icons-material/CheckBox';
@@ -8,24 +8,20 @@ import { TEAM_ICON_DEFAULT } from '../../../../common/staticData';
 import GameService from '../../../../services/game.service';
 import { getFormattedDate } from '../../components/utilities';
 
-const SettingsAcademyTeamGameControl = ({ academy, team, season }) => {
+const SettingsAcademyTeamGameControl = ({ academy, team, season, select, isManager = false }) => {
     const [gameList, setGameList] = useState([]);
     const [loading, setLoading] = useState(false);
-    const [isHided, setIsHided] = useState([]);
-    const isHidedRef = useRef();
+    const [selectedIndex, setSelectedIndex] = useState(-1);
+    const [isHided, setIsHided] = useState([{ game: 0, team: 0 }]);
 
-    isHidedRef.current = isHided;
-
-    const hideGames = (team_id, game_id) => {
-        if (isHidedRef.current.filter((item) => item.game === game_id && item.team === team_id).length > 0) {
+    const hideGames = (game_id, team_id) => {
+        if (isHided.filter((item) => item.game === game_id && item.team === team_id).length > 0) {
             GameService.deleteHideGame(academy.academy_id, game_id, team_id).then((res) => {
-                const newHide = isHidedRef.current.filter((item) => item.game !== game_id && item.team !== team_id);
-
-                setIsHided(newHide);
+                setIsHided((h) => h.filter((item) => item.game !== game_id || item.team !== team_id));
             });
         } else {
             GameService.addHideGame(academy.academy_id, game_id, team_id).then((res) => {
-                let newHide = isHidedRef.current;
+                let newHide = [...isHided];
 
                 newHide = [...newHide, { game: game_id, team: team_id }];
                 setIsHided(newHide);
@@ -34,11 +30,16 @@ const SettingsAcademyTeamGameControl = ({ academy, team, season }) => {
     };
 
     const checkGameTeam = (game_id, team_id) => {
-        return isHidedRef.current.filter((item) => item.game === game_id && item.team === team_id).length > 0;
+        return isHided.filter((item) => item.game === game_id && item.team === team_id).length > 0;
     };
 
     useEffect(async () => {
         setGameList([]);
+
+        if (isManager) {
+            select(null);
+            setSelectedIndex(-1);
+        }
 
         if (team && season) {
             setLoading(true);
@@ -56,12 +57,12 @@ const SettingsAcademyTeamGameControl = ({ academy, team, season }) => {
         }
     }, [team, season]);
 
-    console.log('$$$$$$$$$', gameList);
+    console.log('$$$$$$$$$', gameList, isHided);
 
     return (
         <div className="settings_academy_container">
             <p className="normal-text">Games</p>
-            <div className="team_game_section">
+            <div className="team_game_section" style={{ width: isManager ? '660px' : '720px' }}>
                 {loading ? (
                     <div style={{ width: '100%', height: '80%', zIndex: 9999, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
                         <CircularProgress />
@@ -69,23 +70,34 @@ const SettingsAcademyTeamGameControl = ({ academy, team, season }) => {
                 ) : (
                     <div className="academy_items_container">
                         {gameList.map((item, index) => (
-                            <div key={index} className="team_game_item">
+                            <div
+                                key={index}
+                                className="team_game_item"
+                                style={{ background: isManager && selectedIndex === index ? '#C5EAC6' : 'white' }}
+                                onClick={() => {
+                                    if (isManager) select(item);
+                                }}
+                            >
                                 <p className="normal-text">{getFormattedDate(item.date)}</p>
                                 <div className="game_section">
                                     <img src={item.home_team_image ? item.home_team_image : TEAM_ICON_DEFAULT} />
-                                    <p className="normal-text-strong">{item.home_team_goals}</p>
+                                    <p className="normal-text">{item.home_team_goals}</p>
                                     <p className="normal-text">{item.home_team_name}</p>
-                                    <IconButton color={checkGameTeam(item.id, item.home_team_id) ? 'error' : 'success'} onClick={() => hideGames(item.home_team_id, item.id)}>
-                                        {checkGameTeam(item.id, item.home_team_id) ? <CloseIcon fontSize="large" /> : <CheckBoxIcon fontSize="large" />}
-                                    </IconButton>
+                                    {!isManager && (
+                                        <IconButton color={checkGameTeam(item.id, item.home_team_id) ? 'error' : 'success'} onClick={() => hideGames(item.id, item.home_team_id)}>
+                                            {checkGameTeam(item.id, item.home_team_id) ? <CloseIcon /> : <CheckBoxIcon />}
+                                        </IconButton>
+                                    )}
                                 </div>
                                 <div className="game_section">
                                     <img src={item.away_team_image ? item.away_team_image : TEAM_ICON_DEFAULT} />
-                                    <p className="normal-text-strong">{item.away_team_goals}</p>
+                                    <p className="normal-text">{item.away_team_goals}</p>
                                     <p className="normal-text">{item.away_team_name}</p>
-                                    <IconButton color={checkGameTeam(item.id, item.away_team_id) ? 'error' : 'success'} onClick={() => hideGames(item.away_team_id, item.id)}>
-                                        {checkGameTeam(item.id, item.away_team_id) ? <CloseIcon fontSize="large" /> : <CheckBoxIcon fontSize="large" />}
-                                    </IconButton>
+                                    {!isManager && (
+                                        <IconButton color={checkGameTeam(item.id, item.away_team_id) ? 'error' : 'success'} onClick={() => hideGames(item.id, item.away_team_id)}>
+                                            {checkGameTeam(item.id, item.away_team_id) ? <CloseIcon /> : <CheckBoxIcon />}
+                                        </IconButton>
+                                    )}
                                 </div>
                             </div>
                         ))}
